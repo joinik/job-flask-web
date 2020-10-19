4e0d373e8afcc226c3a0c5f21cfc5ee39c83ad16
joinik <1793844527@qq.com>
2020-10-19 10:56:39 +0800

    '配置数据库，M,V,T，'

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_session import Session
 
 
 
@@ -8,9 +10,29 @@ from flask import Flask
 app = Flask(__name__)
 
 # 注册蓝图
+app.register_blueprint(front)
+
+# app.register_blueprint(user)
+# app.register_blueprint(admin)
+# app.register_blueprint(company)
+# app.register_blueprint(job)
+
+# 加载配置信息
+app.config.from_pyfile("config.ini")
+
+# db初始化配置App
+db.init_app(app)
+
+# Session 对象， 存储到redis 中
+# Session(app)
+
+# 添加数据库迁移工具
+manager = Manager (app)
+# 生成 migrate 对象，用来迁移数据库
+migrate = Migrate (app, db)
+# 添加db 命令
+manager.add_command ('db', MigrateCommand)
 
 
-
-
-
-
+if __name__ == '__main__':
+	manager.run ()
