b917fc17a3b131a290f2f1ca5ebd00d1b73372cb
joinik <1793844527@qq.com>
2020-10-25 20:22:44 +0800

    011-amdmin 视图，功能都已实现

--- views/admin.py
+++ views/admin.py
@@ -1,10 +1,47 @@
+from flask import render_template, request, current_app
 
-from flask import render_template
-
+from forms import admin_required
+from models.index import Company, User, Job
 from . import admin_blu
 import test_data
 
-@admin_blu.route("/")
+
+@admin_blu.route ("/")
 def index():
-	# test_data.run()
-	return render_template("admin/index.html")
+	return render_template ("admin/index.html")
+
+
+@admin_blu.route ('/user')
+@admin_required
+def user():
+	page = request.args.get ('page', default=1, type=int)
+	pagination = User.query.paginate (
+		page=page,
+		per_page=current_app.config['LIST_PER_PAGE'],
+		error_out=False
+	)
+	return render_template ('admin/user.html', pagination=pagination)
+
+
+@admin_blu.route ('/company')
+@admin_required
+def company():
+	page = request.args.get ('page', default=1, type=int)
+	pagination = Company.query.paginate (
+		page=page,
+		per_page=current_app.config['LIST_PER_PAGE'],
+		error_out=False
+	)
+	return render_template ('admin/company.html', pagination=pagination)
+
+
+@admin_blu.route ('/job')
+@admin_required
+def job():
+	page = request.args.get ('page', default=1, type=int)
+	pagination = Job.query.paginate (
+		page=page,
+		per_page=current_app.config['LIST_PER_PAGE'],
+		error_out=False
+	)
+	return render_template ('admin/job.html', pagination=pagination)
