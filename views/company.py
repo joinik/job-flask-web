c3af533061ecda80eff6c062fd2cb4996255a0ed
joinik <1793844527@qq.com>
2020-10-25 18:34:05 +0800

    010-公司 面试，审核，功能都已实现

--- views/company.py
+++ views/company.py
@@ -1,9 +1,23 @@
 from flask import request, render_template, current_app, abort, redirect, url_for, flash
 from flask_login import current_user
 
-from forms import RegisterCompanyForm
+from forms import RegisterCompanyForm, CompanyDetailForm, company_required
 from models.index import Company, Job
 from . import company_blu
+
+
+@company_blu.route("/register", methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    form = RegisterCompanyForm()
+    if form.validate_on_submit():
+        form.create_company()
+        flash('注册成功，请登录', 'success')
+        return redirect(url_for('index.login'))
+
+    return render_template("company/register.html", form=form, active='company_register')
+
 
 @company_blu.route("/")
 def index():
@@ -31,20 +45,68 @@ def detail(company_id):
     return render_template('company/detail.html', company=company_obj, panel='about', active='detail')
 
 
-@company_blu.route("/register", methods=['POST', 'GET'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index.index'))
-    form = RegisterCompanyForm()
+
+@company_blu.route('/account', methods=['GET', 'POST'])
+@company_required
+def edit():
+    form = CompanyDetailForm(obj=current_user)
+    logo_url = current_user.logo
     if form.validate_on_submit():
-        form.create_company()
-        flash('注册成功，请登录', 'success')
-        return redirect(url_for('index.login'))
+        logo_url = form.update_detail(current_user)
+        print(logo_url)
+        flash('企业信息更新成功', 'success')
+    return render_template('company/edit.html', form=form, file_url=logo_url, panel='edit', active='manage')
 
-    return render_template("company/register.html", form=form, active='company_register')
+
+@company_blu.route('/jobs')
+@company_required
+def jobs():
+    page = request.args.get('page', default=1, type=int)
+    pagination = current_user.jobs.order_by(Job.updated_at.desc()).paginate(
+        page=page, per_page=current_app.config['LIST_PER_PAGE'], error_out=False)
+    return render_template('company/jobs.html', pagination=pagination, active='manage', panel='jobs')
 
 
-# def edit():
-#     form = CompanyDetailForm(obj=current_user)
-#     logo_url = current_user.logo
-#     if form.validate_on_submit():
+
+@company_blu.route('/resumes')
+@company_required
+def resumes():
+    status = request.args.get('status', '1')
+    page = request.args.get('page', default=1, type=int)
+    pagination = current_user.delivery.filter_by(status=status).order_by(Delivery.updated_at.desc()).paginate(
+        page=page, per_page=current_app.config['LIST_PER_PAGE'], error_out=False)
+    return render_template('company/resumes.html', pagination=pagination,
+                           active='manage', panel='resumes', status=status)
+
+
+
+
+
+@company_blu.route('/resume/accept')
+@company_required
+def resume_accept():
+    delivery_id = request.args.get('delivery_id')
+    delivery = current_user.delivery.filter_by(id=delivery_id).first_or_404()
+    delivery.accept()
+    db.session.add(delivery)
+    db.session.commit()
+    flash('已列入面试', 'success')
+    return redirect(url_for('company.resumes'))
+
+
+@company_blu.route('/resume/reject')
+@company_required
+def resume_reject():
+    delivery_id = request.args.get('delivery_id')
+    delivery = current_user.delivery.filter_by(id=delivery_id).first_or_404()
+    delivery.reject()
+    db.session.add(delivery)
+    db.session.commit()
+    flash('已列入不合适', 'warning')
+    return redirect(url_for('company.resumes'))
+
+
+@company_blu.errorhandler(413)
+def page_not_found(error):
+    flash('图片大小超过限制', 'warning')
+    return redirect(request.path)
